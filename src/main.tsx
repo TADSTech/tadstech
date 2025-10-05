794751e9a6baffbbe69c10259c5411e0e2120c5c
Michael Tunwashe <motrenewed@gmail.com>
2025-10-05 14:51:33 +0100

    Updated garbage

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import RootComponent from './helpers/nav_helper';
 
-const storedTheme = localStorage.getItem("theme");
-const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-const initialTheme = storedTheme || (prefersDark ? "dark" : "light");
-document.documentElement.classList.toggle("dark", initialTheme === "dark");
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RootComponent />
